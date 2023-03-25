ea2c494504c4aae11af1642db322fff46f21a962
PolinaFaustova <polinamusikerin@gmail.com>
2023-03-25 15:48:59 +0100

    starting to working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
 
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
 const DEBOUNCE_DELAY = 300;
